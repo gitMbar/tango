06217c0fe52bcc2d57aa04a6fe623e3c3fa4dcd8
Michael Barton <mbarton.mail@gmail.com>
2014-09-13 13:25:17 -0700

    Add about page

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Hello, World (From Rango)")
+  return HttpResponse("Hello, World (From Rango)\n<a href='/rango/about'>Learn More</a>")
+
+def about(request):
+  return HttpResponse("This is a project about Rango the magnificent\n<a href='/rango'>Back</a>")
