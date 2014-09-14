672d0ee5ed39afbc79f1992addfb3867d18e4469
Michael Barton <mbarton.mail@gmail.com>
2014-09-13 18:30:48 -0700

    Add template and static paths

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
 # Create your views here.
 
 def index(request):
-  return HttpResponse("Hello, World (From Rango)\n<a href='/rango/about'>Learn More</a>")
+  # Request the context of the input
+  prerender = get_template('rango/index.html')
+  html = prerender.render(Context({'boldmessage': "I'm bolder than bold"}))
+  return HttpResponse(html)
+
 
 def about(request):
   return HttpResponse("This is a project about Rango the magnificent\n<a href='/rango'>Back</a>")
--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -10,8 +10,19 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(__file__)
 
+# Templates
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIRS = (
+        TEMPLATE_PATH,
+    )
+
+#Static Path
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_DIRS = (
+        STATIC_PATH,
+    )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
